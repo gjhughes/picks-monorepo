16140494c051dfbf7613ff2c78ad1916caf47614
gjhughes <ghughes@me.com>
2020-02-05 21:03:02 +0000

    apollo headers

--- packages/native/src/apollo/constants/headers.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  ['Ocp-Apim-Subscription-Key']: '6d1046c9b1d44658a13531f3c1b81073'
-}
--- packages/native/src/apollo/constants/index.ts
+++ packages/native/src/apollo/constants/index.ts
@@ -1,2 +1,1 @@
-export {default as headers} from './headers'
 export {default as uri} from './uri'
